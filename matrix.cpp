1ed72bede758a28000c60ed7b161533a2f5d8c82
zhux0445 <zhux0445@uw.edu>
2020-02-20 12:46:40 -0800

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -10,3 +10,12 @@ SEXP eigenMapMatMult(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::Matr
   return Rcpp::wrap(C);
 }
 
+#include <Rcpp.h>
+using namespace Rcpp;
+
+#include <algorithm>
+
+// [[Rcpp::export]]
+double innerProduct(NumericVector x, NumericVector y) {
+  return std::inner_product(x.begin(), x.end(), y.begin(), 0.0);
+}
