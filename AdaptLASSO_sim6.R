b141f1446fdcfe9a4ef97b81ddfd2fa209927a19
zhux0445 <zhux0445@uw.edu>
2020-10-03 00:41:56 -0700

    Update AdaptLASSO_sim6.R

--- AdaptLASSO_sim6.R
+++ AdaptLASSO_sim6.R
@@ -1191,7 +1191,7 @@ ipest1 <- function(resp,m,r,eta,lam,eps =1e-3,max.tol=1e-7,NonUniform=T,gra00=gr
       }
     }
   }
-  aN=log(log(N))*log(N)
+  aN=log(log(N))*log(J)
   BIC=-2*sum(lh)+l0norm*aN
   
   Bias=c(colSums(gra-Amat1)/10,colMeans(grd-Dmat1))
