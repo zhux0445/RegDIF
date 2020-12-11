3e7cc426a0fe779baf3b4bd940bd85dee3db3acb
zhux0445 <zhux0445@uw.edu>
2020-12-11 04:39:11 -0800

    Update Reg_DIF_Main.R

--- Reg_DIF_Main.R
+++ Reg_DIF_Main.R
@@ -78,7 +78,16 @@ NonUnif_Reg_DIF <- function(resp,m,r,y,N.vec,eta,eps =1e-3,max.tol=1e-7,gra00=NU
     betaold=grbeta
     
     # E STEP
-    LiA=E_step1(resp=resp,N.vec=N.vec,X=X,y=y,G=G,y.allgroup=y.allgroup,Mu.list=Mu.est,Sig.list=Sig.est,gra=gra, grd=grd, grbeta=grbeta, grgamma=grgamma)
+    Mu.est.mat=rbind(Mu.est[1:2],Mu.est[3:4],Mu.est[5:6])
+    Sig.est.slice=array(0,c(2,2,3))
+    Sig.est.slice[,,1]=Sig.est[1:2,];Sig.est.slice[,,2]=Sig.est[3:4,];Sig.est.slice[,,3]=Sig.est[5:6,]
+    if(min(resp)==0)
+    {
+      resp2 <- as.matrix(resp)
+    } else {
+      resp2 <- as.matrix(resp)-1
+    }
+    LiA=E_step1(resp=resp2,Nvec=N.vec,X=X,y=y,G=G,yallgroup=y.allgroup,Mulist=Mu.est.mat,Siglist=Sig.est.slice,gra=gra, grd=grd, grbeta=grbeta, grgamma=grgamma,r=r,J=J,m=m,N1=N1,N2=N2,N3=N3,N=N)
     ng=ngest(LiA=LiA,y=y,Nvec=N.vec,G=G)
     #update mu hat and Sigma hat
     Mu.est=numeric(r*y)
